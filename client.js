03253a238e2c47ffc5cb87633df3c6edfe5635fc
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2018-08-13 21:45:38 -0500

    building out canvas code

--- client.js
+++ client.js
@@ -1,22 +1,57 @@
-const canvas = document.querySelector('#my-canvas')
-const ctx = canvas.getContext('2d')
-
-
 const UNIT_RADIUS = 5
 
-let currentPos = 0
+class Recaman {
+  constructor(ctx, canvas) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.seq = new Set([0]);
+    this.currentSeq = 0;
+    this.nextJumpSize = 1;
+    this.draw();
+  }
+
+  get UNIT_RADIUS() {
+    return UNIT_RADIUS;
+  }
+
+  get currentPos() {
+    return this.currentSeq * this.UNIT_RADIUS;
+  }
+
+  centerLine = () => Math.ceil(this.canvas.height / 2)
+
+
+  draw() {
+    const radius = (this.nextJumpSize / 2) * this.UNIT_RADIUS;
+    let center = this.findNextCenter();
+  }
+
+  findNextCenter(){
+    let center = 0;
+    if (this.canGoBack()) {
+      center = this.currentPos - radius;
+    } else {
+      center = this.currentPos + radius;
+    }
+    return center;
+  }
+
+  canGoBack() {
+    const backNum = this.currentSeq - this.nextJumpSize;
+    return (!this.seq.has(backNum) && backnum > 0);
+  }
+
+}
+
+
+let currentSeq = 0
 let nextJumpSize = 1
+
 
 let recaman = new Set([0]);
 
-window.onresize = e => {
-  console.log(e);
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth;
-  
-}
-
-console.log(recaman);
-
-// draw top on odd, bottom on even
-
+const canvas = document.querySelector('#my-canvas')
+canvas.height = 1000;
+canvas.width = 1500;
+const ctx = canvas.getContext('2d')
+const vis = new Recaman(ctx);
